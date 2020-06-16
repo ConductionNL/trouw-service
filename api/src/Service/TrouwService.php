83041a607787ab4f6f5c159f97da7a71c444c096
Ruben van der Linde <ruben@conduction.nl>
2020-06-16 08:22:31 +0200

    Added the forcing of products

--- api/src/Service/TrouwService.php
+++ api/src/Service/TrouwService.php
@@ -43,6 +43,26 @@ class TrouwService
 
     public function update(array $task, array $resource)
     {
+        // We want to force product shizle
+        $ceremonieOfferId = $this->commonGroundService->getUuidFromUrl($resource['properties']['ceremonie']);
+        switch($ceremonieOfferId) {
+            case "1ba1772b-cc8a-4808-ad1e-f9b3c93bdebf": // Flits huwelijks
+                $resource['properties']['ambtenaar'] = $this->commonGroundService->cleanUrl(['component'=>'pdc','type'=>'offers','id'=>'55af09c8-361b-418a-af87-df8f8827984b']);
+                $resource['properties']['locatie'] = $this->commonGroundService->cleanUrl(['component'=>'pdc','type'=>'offers','id'=>'9aef22c4-0c35-4615-ab0e-251585442b55']);
+                break;
+            case "77f6419d-b264-4898-8229-9916d9deccee": // Gratis trouwen
+                $resource['properties']['ambtenaar'] = $this->commonGroundService->cleanUrl(['component'=>'pdc','type'=>'offers','id'=>'55af09c8-361b-418a-af87-df8f8827984b']);
+                $resource['properties']['locatie'] = $this->commonGroundService->cleanUrl(['component'=>'pdc','type'=>'offers','id'=>'7a3489d5-2d2c-454b-91c9-caff4fed897f']);
+                break;
+            case "2b9ba0a9-376d-45e2-aa83-809ef07fa104": // Eenvoudig trouwen
+                $resource['properties']['ambtenaar'] = $this->commonGroundService->cleanUrl(['component'=>'pdc','type'=>'offers','id'=>'55af09c8-361b-418a-af87-df8f8827984b']);
+                $resource['properties']['locatie'] = $this->commonGroundService->cleanUrl(['component'=>'pdc','type'=>'offers','id'=>'7a3489d5-2d2c-454b-91c9-caff4fed897f']);
+                break;
+            case "bfeb9399-fce6-49b8-a047-70928f3611fb": // Uitgebreid trouwen
+                // In het geval van uitgebreid trouwen hoeven we niks te forceren
+                break;
+        }
+
         // Verlopen reservering
         $task = [];
         $task['code'] = 'verlopen_reservering';
@@ -86,7 +106,7 @@ class TrouwService
         }
 
         // dus ga mail versturen
-        
+
         return $resource;
     }
 
